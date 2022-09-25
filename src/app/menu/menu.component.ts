a3a12dbcc6037e9cf6561e84faf39c6ff646f1f1
Shizz74 <wojciechbajko@gmail.com>
2022-09-25 22:48:15 +0200

    Change menu more small devce friendly part 1

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  activeElement: string = 'list';
+
+
+  constructor(private elRef: ElementRef) { }
 
   ngOnInit(): void {
   }
+
+  setActive(active: string) {
+    this.activeElement = active; 
+  }
+
 
 }
